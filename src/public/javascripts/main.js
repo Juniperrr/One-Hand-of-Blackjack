60367d0a8e64db78bcbe5f908f415e6414636e1b
Juniperrr <sohnjunhee2011@gmail.com>
2019-11-13 22:52:12 -0500

    created html elements

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,12 +1,16 @@
+// Referred to 
+// 'https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript'
+// in order to learn how to implement BlackJack game.
+// Code may resemble some parts of the learning material.
+
 const suits = ["S", "D", "C", "H"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let deck = new Array();
 const players = new Array();
-let currentPlayer = 0;
+// let currentPlayer = 0;
 
 // Generate a deck of 52 unshuffled cards and assigning points.
 function generateDeck(callback) {
-    // 3
     deck = new Array();
     for(let i = 0; i < suits.length; i++) { // 4
 		for(let x = 0; x < values.length; x++) { // 13
@@ -22,16 +26,13 @@ function generateDeck(callback) {
             }
             const card = { suit: suits[i], value: values[x], point: pnt };
             deck.push(card);
-            //4
 		}
     }
-    // 5
     callback(); // shuffle.
 }
 
 // Shuffling the deck.
 function shuffle() {
-    // 6
 	// for 200 turns, and switch the values of two random cards
 	for (let i = 0; i < 200; i++) {
 		const card1 = Math.floor((Math.random() * deck.length));
@@ -40,12 +41,10 @@ function shuffle() {
 		deck[card1] = deck[card2];
 		deck[card2] = tmp;
     }
-    //7
 }
 
 // Declare 2 players, who will get 2 cards assigned.
 function generatePlayers() {
-    // 8
     const user = {
         name: 'user',
         points: 0,
@@ -58,39 +57,6 @@ function generatePlayers() {
         hands: new Array(),
     };
     players.push(computer);
-    // 9
-}
-
-// deal the hand (to me and compture) + pop and push if specified
-function dealHands(startValues) {
-    // 10
-    // push the speciied cards to the front
-    for (let i = startValues.length-1; i >= 0; i--) {
-        let startPoint;
-        if (startValues[i] === "A") {
-            startPoint = 11; // or 1.
-        }
-        else if (startValues[i] === "J" || startValues[i] === "Q" || startValues[i] === "K") {
-            startPoint = 10;
-        }
-        else {
-            startPoint = parseInt(startValues[i]);
-        }
-        const startCard = { suit: "hearts", value: startValues[i], point: startPoint };
-        deck.unshift(startCard); //1,2,3,4,5,6,7
-    }
-    //11
-    // deal hands to the 2 players
-    for(let i = 0; i < 2; i++) { // 2 cards
-        for (let x = 0; x < players.length; x++) { // 2 players
-            const card = deck[0];
-            deck.shift(); 
-            players[x].hands.push(card); 
-            // renderCard(card, x);
-            updateScores();
-        }
-    }
-    // updateDeck();
 }
 
 function updateScores() {
@@ -107,92 +73,131 @@ function updateScores() {
     }
 }
 
-function getImgAttributeName(card) {
-    const cardPoint = card.point + "";
-    const cardSuit = card.suit;
-    const attributeName = cardPoint + cardSuit + ".png";
-    return attributeName;
+// deal the hand (to me and compture) + pop and push if specified
+function dealHands(startValues) {
+    // push the specified cards to the front
+    for (let i = startValues.length-1; i >= 0; i--) {
+        let startPoint;
+        if (startValues[i] === "A") {
+            startPoint = 11; // or 1.
+        }
+        else if (startValues[i] === "J" || startValues[i] === "Q" || startValues[i] === "K") {
+            startPoint = 10;
+        }
+        else {
+            startPoint = parseInt(startValues[i]);
+        }
+        const startCard = { suit: "hearts", value: startValues[i], point: startPoint };
+        deck.unshift(startCard); //1,2,3,4,5,6,7
+    }
+    // deal hands to the 2 players
+    for(let i = 0; i < 2; i++) { // 2 cards
+        for (let x = 0; x < players.length; x++) { // 2 players
+            const card = deck[0];
+            deck.shift(); 
+            players[x].hands.push(card); 
+            // should render cards?
+            updateScores();
+        }
+    }
+    // should update deck?
 }
 
-function setImgAttribute(card) {
-    const cardPoint = card.point + "";
-    const cardSuit = card.suit;
-    const attributeName = "/public/img/" + cardPoint + cardSuit + ".png";
-    card.setAttribute("src", attributeName);
-    card.setAttribute("width", "53");
-    card.setAttribute("height", "85");
+function cardsDisplay(userCards, computerCards) {
+    for (let p = 0; p < players.length; p++) {
+        if (p === 0) {
+            for (let h = 0; players[0].hands.length; h++) {
+                const card = players[0].hands[h];
+                const cardImg = document.createElement('img');
+                cardImg.src = './public/img/' + card.value + card.suit + ".png";
+                userCards.appendChild(cardImg);
+            }
+        }
+        else {
+            for (let h = 0; players[1].hands.length; h++) {
+                const card = players[1].hands[h];
+                const cardImg = document.createElement('img');
+                cardImg.src = './public/img/' + card.value + card.suit + ".png";
+                computerCards.appendChild(cardImg);
+            }
+        }
+    }
 }
 
 // Display user name/scores, cards, hit/stand
-function displayInitialUI () {
+function gameInterface () {
+    const gameDiv = document.querySelector('div[class=game]');
+
+	const userDiv = document.createElement('div');
+    userDiv.id='userDiv';
+    const userTitle = document.createElement('h3');	
+    userTitle.id='userTitle';
+    const userCards = document.createElement('div');
+    userCards.id='userCards';
+    userDiv.appendChild(userTitle);
+    userDiv.appendChild(userCards);
+
+    const computerDiv = document.createElement('div');
+	computerDiv.id='computerDiv';
+    const computerTitle = document.createElement('h3');	
+	computerTitle.id='computerTitle';
+    const computerCards = document.createElement('div');
+	computerCards.id='computerCards';
+    computerDiv.appendChild(computerTitle);
+	computerDiv.appendChild(computerCards);
+
+    const hitBtn = document.createElement('button');
+	const standBtn = document.createElement('button');
+	const bottomBlock = document.createElement('div');
+	hitBtn.id='hitBtn';
+	standBtn.id='standBtn';
+    bottomBlock.id='bb';
+    
+    bottomBlock.appendChild(hitBtn);
+	bottomBlock.appendChild(standBtn);
+    hitBtn.textContent = 'HIT';
+    standBtn.textContent = 'STAND';
+    
+    gameDiv.appendChild(userDiv);
+    gameDiv.appendChild(computerDiv);
+    gameDiv.appendChild(bottomBlock);
+    
+    // firstR(c,p,cardList,pCards,cCards);
+    cardsDisplay(userCards, computerCards);
+    updateScores();
+    userTitle.textContent = 'Player Hand - Total: '+ players[0].points;
+	computerTitle.textContent = 'Computer Hand - Total: ?';
+	// hitBtn.onclick = ()=>hit(deck,p,pCards,pTotal,pTitle);
+	// standBtn.onclick = ()=>stand(cardList,cCards,cTotal,cTitle);
+	// if(total(p,'p')===21){
+	// 	gameOver();
+	// }
+//----------------------------------------------
+    /*
     // display user name
     for (let p = 0; players.length; p++) {
         const divPlayer = document.createElement('div'); // name, score, cards
 
-        const playerName = document.createTextNode(players[p].name);
+        const playerName = players[p].name;
         divPlayer.appendChild(playerName);
-        const playerScore = document.createTextNode(players[p].points);
+        const playerScore = players[p].points;
         divPlayer.appendChild(playerScore);
-
+        console.log('done');
         const numHands = players[p].hands.length;
         const playerHand2ndLast = players[p].hands[numHands-2];
         const playerHandLast = players[p].hands[numHands-1];
-        const playerCard1 = document.createElement(playerHand2ndLast); // displays the 2nd latest
+        const playerCard1 = document.createElement("playerCard1"); // displays the 2nd latest
         setImgAttribute(playerCard1);
         divPlayer.appendChild(playerCard1);
-        const playerCard2 = document.createElement(playerHandLast); // displays the latest 
+        const playerCard2 = document.createElement("playerCard1"); // displays the latest 
         setImgAttribute(playerCard2);
         divPlayer.appendChild(playerCard2);
 
         const currDiv = document.getElementById("cdiv1");
         document.body.insertBefore(divPlayer, currDiv);
-        /*
-        // const divPlayerId = document.createElement('div');
-
-        divPlayer.id = 'player_' + p;
-
-        // divPlayerId.innerHTML = players[p].ID;
-        divPlayer.appendChild(divPlayerId);
-        document.getElementById('players').appendChild(divPlayer);
-        */
     }
-    
+    */
 }
-
-function renderDeck() {
-    const deck = shuffle;
-	document.getElementById('deck').innerHTML = ''; //ID="deck"
-	for(let i = 0; i < deck.length; i++) {
-		const card = document.createElement("div");
-		let icon = '';
-		if (deck[i].Suit === 'hearts') {
-            icon='?';
-        }
-		else if (deck[i].Suit === 'spades') {
-            icon = '?';
-        }
-		else if (deck[i].Suit === 'diamonds') {
-            icon = '?';
-        }
-		else {
-            icon = '?';
-        }
-		card.innerHTML = deck[i].Value + '' + icon;
-		card.className = 'card';
-	document.getElementById("deck").appendChild(card);
-	}
-}
-
-
-/*
-btn.addEventListner('click', function(evt) {
-        const textInput = documen.querySelector('input[type=text]');
-        const teaname = textInput.value;
-        const url = `https://api.github.com/users/${username}/repos`;
-        getRepose(url);
-        evt.preventDefault(); // actually not submitting the fom!
-    });
-*/
 
 // main.js
 function main() {
@@ -207,7 +212,7 @@ function main() {
         generateDeck(shuffle);
         generatePlayers();
         dealHands(startValues);
-        displayInitialUI();
+        // displayInitialUI();
     });
     // 1
     // dealHands(startValues);
@@ -221,6 +226,7 @@ module.exports = {
     generatePlayers: generatePlayers,
     dealHands: dealHands,
     updateScores: updateScores,
-    displayInitialUI: displayInitialUI,
-    renderDeck: renderDeck,
+    gameInterface: gameInterface,
+    cardsDisplay: cardsDisplay,
+    // displayInitialUI: displayInitialUI,
   };
